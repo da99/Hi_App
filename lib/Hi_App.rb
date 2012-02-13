bfb188dd6f22c3cd95f885676e38cd5fa0133c40
da99 <i-hate-spam-45671204@mailinator.com>
2012-02-13 11:24:52 -0600

    First commit: Gem created.

---
+++ lib/Hi_App.rb
@@ -0,0 +1,7 @@
+require 'Hi_App/version'
+
+class Hi_App
+  
+  
+  
+end # === class Hi_App
---
+++ spec/main.rb
@@ -0,0 +1,14 @@
+
+require File.expand_path('spec/helper')
+require 'Hi_App'
+require 'Bacon_Colored'
+
+
+# ======== Include the tests.
+if ARGV.size > 1 && ARGV[1, ARGV.size - 1].detect { |a| File.exists?(a) }
+  # Do nothing. Bacon grabs the file.
+else
+  Dir.glob('spec/tests/*.rb').each { |file|
+    require File.expand_path(file.sub('.rb', '')) if File.file?(file)
+  }
+end
